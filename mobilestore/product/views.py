91a3f6e97e8e76f50812ee52230092610672b959
kul-deep <kuldeepchoudhary501@gmail.com>
2021-05-02 22:47:48 +0530

    final commit

--- mobilestore/product/views.py
+++ mobilestore/product/views.py
@@ -5,25 +5,24 @@ from django.contrib import messages
 # Create your views here.
 def productreg(request):
     if request.method=='POST':
-        # if request.POST.get('name') and request.POST.get('brand') and request.POST.get('ram') and request.POST.get('storage') and request.POST.get('price') and request.POST.get('stock'):
-        #     saverecord=Phone()
-        #     saverecord.name=request.POST.get('name')
-        #     saverecord.brand=request.POST.get('brand')
-        #     saverecord.ram=request.POST.get('ram')
-        #     saverecord.storage=request.POST.get('storage')
-        #     saverecord.price=request.POST.get('price')
-        #     saverecord.stock=request.POST.get('stock')
-        #     saverecord.save()
-        #     messages.success(request, 'New Mobile Registration Detials Saved Successfully')
         name = request.POST['name']
         brand = request.POST['brand']
         ram = request.POST['ram']
         storage = request.POST['storage']
         price = request.POST['price']
         stock = request.POST['stock']
-        addPhone = Phone(name=name, brand=brand, ram=ram, storage=storage, price=price, stock=stock)
-        addPhone.save()
-        return render(request, 'index.html')
+
+        if int(stock) < 0:
+            value = {
+                "msg": "Please enter a valid stock."
+            }
+        else:
+            value = {
+                "msg" : "Phone details added successfully."
+            }
+            addPhone = Phone(name=name, brand=brand, ram=ram, storage=storage, price=price, stock=stock)
+            addPhone.save()
+        return render(request, 'index.html', value)
     else:
         return render(request, 'index.html')
 def productretrieve(request):
